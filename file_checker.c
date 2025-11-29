3e3852cdcc8fd6af0f80dbf4964c96485e637f73
AndreiPetrut8 <apa80000@gmail.com>
2025-11-29 22:37:26 +0200

    Client +2 buttons

---
+++ file_checker.c
@@ -0,0 +1,103 @@
+#include <ncurses.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MAX_ITEMS 2048
+#define PORT 8081
+
+char current_path[1024];
+char *items[MAX_ITEMS];
+int item_count = 0;
+int selected = 0;
+
+void load_directory(const char *path) {
+    DIR *dir;
+    struct dirent *entry;
+
+    for (int i = 0; i < item_count; i++)
+        free(items[i]);
+    item_count = 0;
+    selected = 0;
+
+    dir = opendir(path);
+    if (!dir) return;
+
+    while ((entry = readdir(dir)) != NULL) {
+        items[item_count++] = strdup(entry->d_name);
+        if (item_count >= MAX_ITEMS) break;
+    }
+
+    closedir(dir);
+}
+
+int main() {
+    strcpy(current_path, "/");
+
+    initscr();
+    noecho();
+    curs_set(FALSE);
+    keypad(stdscr, TRUE);
+    //comm
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0) {
+      perror("socket creation failed");
+      exit(-1);
+    }
+
+    struct sockaddr_in address = {0};
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
+
+    if (connect(socket_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+      perror("Connection failed");
+      exit(-1);
+    }
+
+   
+    //end comm
+   
+
+    load_directory(current_path);
+
+    while (1) {
+        clear();
+        mvprintw(0, 0, "Exploring: %s", current_path);
+	while((int n = read(socket_fd,current_path, sizeof(current_path)-1)) > 0)
+      {
+	current_path[n] = '\0';
+      }
+
+    close(socket_fd);
+
+        for (int i = 0; i < item_count; i++) {
+            if (i == selected)
+                attron(A_REVERSE);
+
+            if (is_directory(current_path, items[i]))
+                mvprintw(i + 2, 2, "[%s]", items[i]);
+            else
+                mvprintw(i + 2, 2, "%s", items[i]);
+
+            if (i == selected)
+                attroff(A_REVERSE);
+        }
+
+        refresh();
+
+        int ch = getch();
+
+        if (ch == 'q') {
+            break;
+        }
+    }
+
+    endwin();
+    return 0;
+}
